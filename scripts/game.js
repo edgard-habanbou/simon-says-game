9a797d83b1f62062ad7b0b0079faa33b54881f7f
edgard <edgard.habanbou@hotmail.com>
2023-11-10 23:38:27 +0200

    Added animation and sound when user presses a color

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+$("#green").click(function () {
+  UserPressedAnimationAndSound("green");
+});
+
+$("#red").click(function () {
+  UserPressedAnimationAndSound("red");
+});
+
+$("#yellow").click(function () {
+  UserPressedAnimationAndSound("yellow");
+});
+
+$("#blue").click(function () {
+  UserPressedAnimationAndSound("blue");
+});
+
+function UserPressedAnimationAndSound(color) {
+  $("#" + color).addClass("pressed");
+  setTimeout(function () {
+    $("#" + color).removeClass("pressed");
+  }, 50);
+  var audio = new Audio("./sounds/" + color + ".mp3");
+  audio.play();
+}
